9f8701af8a92c54c91ad90800f54f0c0dd5cf530
Poroh <meta.poroh.kun@gmail.com>
2018-04-24 00:06:52 +0300

    ship fly

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class CameraHandler : MonoBehaviour
+{
+    private Camera _camera;
+    public Camera Camera
+    {
+        get
+        {
+            if (_camera == null)
+                _camera = GetComponent<Camera>();
+            return _camera;
+        }
+    }
+
+    Plane _plane = new Plane(Vector3.back, Vector3.zero);
+    
+    public Vector3 ScreenToFrustumPoint(Vector3 point)
+    {
+        var ray = this.Camera.ScreenPointToRay(point);
+        float enter;
+        _plane.Raycast(ray, out enter);
+        return ray.GetPoint(enter);
+    }
+
+    void Update()
+    {
+        Vector3[] frustumCorners = new Vector3[4];
+        this.Camera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), -this.Camera.transform.position.z, Camera.MonoOrStereoscopicEye.Mono, frustumCorners);
+
+        for (int i = 0; i < frustumCorners.Length; i++)
+            Debug.DrawLine(frustumCorners[i] + this.Camera.transform.position, frustumCorners[(i + 1) % frustumCorners.Length] + this.Camera.transform.position, Color.blue);
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 1f;
+    [SerializeField]
+    private float _accelerationMod = 2f;
+    [SerializeField]
+    private float _rotateSpeed = 1f;
+    [SerializeField]
+    private CameraHandler _camera;
+    [SerializeField]
+    private Transform _ship;
+
+    private Vector2 _direction = Vector2.up;
+    private Vector2 _course = Vector2.up;
+    private bool _acceleration = false;
+
+    private void Update()
+    {
+        var mousePos = _camera.ScreenToFrustumPoint(Input.mousePosition);
+        _course = (mousePos - _ship.position).normalized;
+        _direction = Vector3.RotateTowards(_direction, _course, _rotateSpeed * Time.deltaTime, 0f);
+
+        Debug.DrawLine(_ship.position, mousePos, Color.red);
+        Debug.DrawRay(_ship.position, _direction * 5f, Color.green);
+
+        _ship.rotation = Quaternion.LookRotation(_direction, Vector3.forward);
+        _ship.position += (Vector3)(_direction * _speed * Time.deltaTime * (_acceleration ? 1f : _accelerationMod));
+
+
+        //transform.position = mousePos;
+    }
+}
