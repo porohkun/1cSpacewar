7ef990199d9c2827fc4390a5ecc5b80182d50c90
Poroh <meta.poroh.kun@gmail.com>
2018-04-24 02:09:32 +0300

    explosions

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer _renderer;
+    [SerializeField]
+    private Material _safeExplosionMaterial;
+    [SerializeField]
+    private Material _dangerExplosionMaterial;
+    [SerializeField]
+    private float _safeTime = 1f;
+    [SerializeField]
+    private float _fullTime = 3f;
+    [SerializeField]
+    private float _explodeSpeed = 15f;
+
+    public bool IsSafe { get { return _elapsedTime < _safeTime; } }
+
+    private float _diameter;
+    private float _elapsedTime;
+
+    public void Begin()
+    {
+        _elapsedTime = 0f;
+        _diameter = 0f;
+        UpdateVisual();
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        _diameter += _explodeSpeed * Time.deltaTime;
+        UpdateVisual();
+
+        if (_elapsedTime >= _fullTime)
+            gameObject.SetActive(false);
+    }
+
+    private void UpdateVisual()
+    {
+        _renderer.material = _elapsedTime < _safeTime ? _safeExplosionMaterial : _dangerExplosionMaterial;
+        transform.localScale = Vector3.one * _diameter;
+    }
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,23 +19,75 @@ public class Game : MonoBehaviour
     [SerializeField]
     private float _ricochetTime = 0.3f;
     [SerializeField]
+    private float _startTimeBetweenExplosions = 5f;
+    [SerializeField]
+    private float _explosionRecalcTime = 5f;
+    [SerializeField]
+    private float _explosionAccelerationTimeMod = 0.9f;
+    [SerializeField]
     private CameraHandler _camera;
     [SerializeField]
     private Transform _ship;
+    [SerializeField]
+    private Explosion _explosionPrefab;
 
     private Vector2 _direction = Vector2.up;
     private Vector2 _course = Vector2.up;
     private ValueKeeper _acceleration = new ValueKeeper();
     private ValueKeeper _ricochet = new ValueKeeper();
     private Rect _field;
+    private float _gameTime;
+    private float _timeBetweenExplosions = 5f;
+    private List<Explosion> _explosions = new List<Explosion>();
 
     private void Start()
     {
         _field = _camera.GetFrustumRect();
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        _timeBetweenExplosions = _startTimeBetweenExplosions;
+        StartCoroutine(ExplosionAccelerationRoutine());
+        StartCoroutine(ExplosionCreationRoutine());
+    }
+
+    private IEnumerator ExplosionAccelerationRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_explosionRecalcTime);
+            _timeBetweenExplosions *= _explosionAccelerationTimeMod;
+        }
+    }
+
+    private IEnumerator ExplosionCreationRoutine()
+    {
+        while (true)
+        {
+            Explosion expl = GetExplosion();
+            expl.gameObject.SetActive(true);
+            expl.Begin();
+            expl.transform.position = new Vector2(Random.Range(_field.xMin, _field.xMax), Random.Range(_field.yMin, _field.yMax));
+            yield return new WaitForSeconds(_timeBetweenExplosions);
+        }
+    }
+
+    private Explosion GetExplosion()
+    {
+        Explosion explosion = _explosions.FirstOrDefault(e => !e.gameObject.activeSelf);
+        if (explosion == null)
+        {
+            explosion = Instantiate(_explosionPrefab, transform);
+            _explosions.Add(explosion);
+        }
+        return explosion;
     }
 
     private void Update()
     {
+        _gameTime += Time.deltaTime;
         ValueKeepersUpdate();
         ShipMovement();
         RicochetChecking();
